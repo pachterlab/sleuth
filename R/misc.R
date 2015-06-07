8c1ed949ecb91c19ddb8ac2836e401927d5fc2fb
Harold Pimentel <haroldpimentel@gmail.com>
2015-06-06 21:06:55 -0700

    remove all files for gh-pages

--- R/misc.R
+++
@@ -1,8 +0,0 @@
-#' @export
-get_quantile <- function(data, which_col, lwr, upr) {
-  data <- as.data.frame(data)
-  data$cdf <- data[,which_col] %>%
-    ecdf(.)(.) %>%
-    ifelse(lwr <= . & . <= upr)
-  data
-}
