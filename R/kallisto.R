d8acd5e178739e3936de1c1363c813f2ce7e6fdc
Harold Pimentel <haroldpimentel@gmail.com>
2015-08-13 22:01:50 -0700

    delete unused functions in kallisto.R

--- R/kallisto.R
+++ R/kallisto.R
@@ -1,14 +1,3 @@
-#' kallisto coefficient of variation
-#'
-#' cv
-#'
-#' @param kal a kallisto object
-#' @return a data.frame with the coefficient of variation
-coef_var <- function(kal) {
-    stopifnot(is(kal, "kallisto"))
-
-}
-
 #' @export
 print.kallisto <- function(obj) {
   cat("\tkallisto object\n")
