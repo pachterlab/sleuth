ce4e6853219e415ce365d1b8a66104b7af02dbfd
Harold Pimentel <haroldpimentel@gmail.com>
2015-10-24 15:07:32 -0700

    update size factors to multiply to 1 #vc

--- R/sleuth.R
+++ R/sleuth.R
@@ -305,10 +305,13 @@ norm_factors <- function(mat) {
   nz <- apply(mat, 1, function(row) !any(round(row) == 0))
   mat_nz <- mat[nz,]
   p <- ncol(mat)
-  geo_means <- exp(apply(mat_nz, 1, function(row) (1/p) * sum(log(row)) ))
+  geo_means <- exp(apply(mat_nz, 1, function(row) mean(log(row)) ))
   s <- sweep(mat_nz, 1, geo_means, `/`)
 
-  apply(s, 2, median)
+  sf <- apply(s, 2, median)
+  scaling <- exp( (-1/p) * sum(log(sf)))
+  
+  sf * scaling
 }
 
 # Summarize many bootstrap objects
