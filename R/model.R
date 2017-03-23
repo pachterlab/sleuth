872aeb1dc9f1d2f6fde8427c3905b85debc24903
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2017-03-23 07:50:04 -0500

    update documentation, including for models function

--- R/model.R
+++ R/model.R
@@ -35,7 +35,7 @@ print.sleuth_model <- function(obj) {
 #'
 #' View which models have been fit
 #'
-#' @param obj
+#' @param obj a \code{'sleuth'} or \code{'sleuth_model'} object
 #' @export
 models <- function(obj, ...) {
   UseMethod('models')
