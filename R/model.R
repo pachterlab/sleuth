4f93c1804e444bcfa98fab5b676c092cc68132d4
Harold Pimentel <haroldpimentel@gmail.com>
2018-03-12 10:07:55 -0700

    fix minor escaping issue #vc

--- R/model.R
+++ R/model.R
@@ -26,7 +26,7 @@
 print.sleuth_model <- function(obj) {
   cat('formula: ', deparse(obj$formula), '\n')
   cat('data modeled: ', obj$which_var, '\n')
-  cat('transform sync'ed: ', obj$transform_sync, '\n')
+  cat('transform sync\'ed: ', obj$transform_sync, '\n')
   cat('coefficients:\n')
   cat(paste0('\t', colnames(obj$design_matrix), '\n'))
 
