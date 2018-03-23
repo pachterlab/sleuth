6991c6a2f30b096878a3e4fe803737d4c711791b
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2018-03-23 09:56:53 -0500

    use data.table to speed up spread_abundance_by function

--- R/sleuth.R
+++ R/sleuth.R
@@ -718,14 +718,15 @@ sleuth_summarize_bootstrap_col <- function(obj, col, transform = identity) {
 # @export
 spread_abundance_by <- function(abund, var, which_order) {
   # var <- lazyeval::lazy(var)
-  var_spread <- abund %>%
-    select_("target_id", "sample", var) %>%
-    tidyr::spread_("sample", var) %>%
-    as.data.frame(stringsAsFactors = FALSE)
-
+  abund <- data.table::as.data.table(abund)
+  var_spread <- data.table::dcast(abund, target_id ~ sample, value.var = var)
+  # there is a discrepancy between data table's sorting of character vectors
+  # and how tidyr previously (or the order function) sorts character vectors
+  # so next step is needed to make sure the order is correct
+  var_spread <- var_spread[order(var_spread$target_id), ]
+  var_spread <- as.data.frame(var_spread, stringsAsFactors = FALSE)
   rownames(var_spread) <- var_spread$target_id
   var_spread["target_id"] <- NULL
-
   result <- as.matrix(var_spread)
 
   result[, which_order, drop = FALSE]
